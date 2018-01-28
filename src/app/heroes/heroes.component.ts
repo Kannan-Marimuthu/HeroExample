4ba14b542c45d31326ed8293521345c3ae93ff81
Kannan Marimuthu <kannanmcatce@gmail.com>
2018-01-27 23:56:42 -0500

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero = {
+    id: 1,
+    name: 'Kannan'
+  };
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  heros: Hero[];
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heros = heroes);
+  }
+
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
